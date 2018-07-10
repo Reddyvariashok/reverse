0a710de50b0d48e36de6ece627643c3c7b5da325
Reddyvariashok <40815856+Reddyvariashok@users.noreply.github.com>
2018-07-10 09:22:13 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+long  num, reverse = 0, temp, remainder;
+
+printf("Enter the number\n");
+
+scanf("%d", &num);
+
+temp = num;
+
+while (num > 0)
+
+{
+
+remainder = num % 10;
+
+reverse = reverse * 10 + remainder;
+
+num /= 10;
+
+}
+
+printf("Given number = %d\n", temp);
+
+printf("Its reverse is = %d\n", reverse);
+
+}
